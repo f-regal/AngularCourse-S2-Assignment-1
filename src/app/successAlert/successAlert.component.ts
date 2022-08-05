9d469a65b95b84dba3de5d887054c3519eb3a54b
Faysal Regal <faysalregal@gmail.com>
2022-08-05 18:14:55 +0100

    added success and warning component

---
+++ src/app/successAlert/successAlert.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success-component',
+  templateUrl: './successAlert.component.html',
+  styleUrls: ['./successAlert.component.css']
+})
+
+
+export class SuccessAlertComponent  {
+
+
+}
---
+++ src/app/warningAlert/warningAlert.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-component',
+  templateUrl: './warningAlert.component.html',
+  styleUrls: ['./warningAlert.component.css']
+})
+export class WarningAlertComponent {
+
+  
+
+
+}
